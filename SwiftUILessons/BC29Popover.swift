384deba5dabc71a500c9f237ff11399f98db7895
Prince Avecillas <avecillasprince@yahoo.com>
2023-05-09 21:10:11 -0400

    Adding more

---
+++ SwiftUILessons/BC29Popover.swift
@@ -0,0 +1,74 @@
+//
+//  BC29Popover.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 5/7/23.
+//
+
+import SwiftUI
+
+struct BC29Popover: View {
+    @State var showNewScreen: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.orange
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Button("Button") {
+                    showNewScreen.toggle()
+                }
+                .font(.largeTitle)
+                Spacer()
+            }
+//            .sheet(isPresented: $showNewScreen) {
+//                BC29NewScreen()
+//            }
+            
+//            ZStack {
+//                if showNewScreen {
+//                    BC29NewScreen(showNewScreen: $showNewScreen)
+//                        .padding(.top, 100)
+//                        .transition(.move(edge: .bottom))
+//                        .animation(.spring())
+//                }
+//            }
+//            .zIndex(2.0)
+            
+            BC29NewScreen(showNewScreen: $showNewScreen)
+                .padding(.top, 100)
+                .offset(y: showNewScreen ? 0 : UIScreen.main.bounds.height)
+                .animation(.spring())
+        }
+    }
+}
+
+struct BC29NewScreen: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var showNewScreen: Bool
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.purple
+                .edgesIgnoringSafeArea(.all)
+            
+            Button {
+//                presentationMode.wrappedValue.dismiss()
+                showNewScreen.toggle()
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+
+                .padding(20)            }
+
+        }
+    }
+}
+
+struct BC29Popover_Previews: PreviewProvider {
+    static var previews: some View {
+        BC29Popover()
+    }
+}
---
+++ SwiftUILessons/BC30Navigation.swift
@@ -0,0 +1,67 @@
+//
+//  BC30Navigation.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 5/7/23.
+//
+
+import SwiftUI
+
+struct BC30Navigation: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                NavigationLink("Hello", destination: BC30MyOtherScreen())
+                
+                Text("Hello")
+                Text("Hello")
+                Text("Hello")
+            }
+            .navigationTitle("All Inboxes")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbar(.hidden)
+            .navigationBarItems(
+                leading: HStack {
+                    Image(systemName: "person.fill")
+                    Image(systemName: "flame.fill")
+                },
+                
+                trailing: NavigationLink(destination: {
+                    BC30MyOtherScreen()
+                }, label: {
+                    Image(systemName: "gear")
+                })
+            )
+            .accentColor(.red)
+        }
+    }
+}
+
+struct BC30MyOtherScreen: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            Color.green.edgesIgnoringSafeArea(.all)
+                .navigationTitle("Green")
+//                .toolbar(.hidden)
+            
+            VStack {
+                Button("Back Button") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                                
+                NavigationLink("Click") {
+                    Text("3rd screen")
+                }
+            }
+        }
+    }
+}
+
+struct BC30Navigation_Previews: PreviewProvider {
+    static var previews: some View {
+        BC30Navigation()
+    }
+}
