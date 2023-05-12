8f5730681d616382470263fab4665eda58160efc
Prince Avecillas <avecillasprince@yahoo.com>
2023-05-11 20:36:12 -0400

    Adding lesson 31

---
+++ SwiftUILessons/BC31EditLists.swift
@@ -0,0 +1,78 @@
+//
+//  BC31EditLists.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 5/11/23.
+//
+
+import SwiftUI
+
+struct BC31EditLists: View {
+    @State var fruits: [String] = [
+        "apple", "orange", "banana", "peach"
+    ]
+    
+    @State var veggies: [String] = [
+        "tomato", "potato", "carrot"
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section (
+                    header:
+                        HStack {
+                            Text("Fruits")
+                            Image(systemName: "flame.fill")
+                        }
+                        .font(.headline)
+                        .foregroundColor(.orange)
+                ) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+                    .listRowBackground(Color.blue)
+                }
+                
+                Section(header: Text("Veggies")) {
+                    ForEach(veggies, id: \.self) { veggies in
+                        Text(veggies.capitalized)
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Grocery List")
+            .navigationBarItems(leading: EditButton(), trailing: addButton)
+            .accentColor(.purple)
+        }
+    }
+    
+    var addButton: some View {
+        Button("Add") {
+            add()
+        }
+    }
+    
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    
+    func move(indices: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func add() {
+        fruits.append("Coconut")
+    }
+}
+
+struct BC31EditLists_Previews: PreviewProvider {
+    static var previews: some View {
+        BC31EditLists()
+    }
+}
