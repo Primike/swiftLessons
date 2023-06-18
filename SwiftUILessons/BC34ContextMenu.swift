9263ef995f6aef37b0c5824b836cb959aedd27b0
Prince Avecillas <avecillasprince@yahoo.com>
2023-06-18 08:22:08 -0400

    Adding more lessons

---
+++ SwiftUILessons/BC34ContextMenu.swift
@@ -0,0 +1,57 @@
+//
+//  BC34ContextMenu.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 6/17/23.
+//
+
+import SwiftUI
+
+struct BC34ContextMenu: View {
+    
+    @State var backgroundColor: Color = .blue
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(systemName: "house.fill")
+                .font(.title)
+            
+            Text("Swiftful Thinking")
+                .font(.headline)
+            
+            Text("How to use Context Menu")
+                .font(.subheadline)
+        }
+        .foregroundColor(.white)
+        .padding(30)
+        .background(backgroundColor.cornerRadius(30))
+        .contextMenu {
+            Button {
+                backgroundColor = .yellow
+            } label: {
+                Label("Share Post", systemImage: "flame.fill")
+            }
+
+            Button {
+                backgroundColor = .red
+            } label: {
+                Text("Report Post")
+            }
+            
+            Button {
+                backgroundColor = .green
+            } label: {
+                HStack {
+                    Text("Like Post")
+                    Image(systemName: "heart.fill")
+                }
+            }
+        }
+    }
+}
+
+struct BC34ContextMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        BC34ContextMenu()
+    }
+}
---
+++ SwiftUILessons/BC35TextField.swift
@@ -0,0 +1,64 @@
+//
+//  BC35TextField.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 6/17/23.
+//
+
+import SwiftUI
+
+struct BC35TextField: View {
+    
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                //                .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.red)
+                    .font(.headline)
+                
+                Button {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                } label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ? .blue : .gray)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                Spacer()
+            }
+        }
+        .padding()
+        .navigationTitle("TextField Bootcamp!")
+    }
+    
+    func textIsAppropriate() -> Bool {
+        textFieldText.count >= 3 ? true : false
+    }
+    
+    func saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+struct BC35TextField_Previews: PreviewProvider {
+    static var previews: some View {
+        BC35TextField()
+    }
+}
