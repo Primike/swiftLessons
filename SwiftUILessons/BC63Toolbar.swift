fe66c6e00ee377a7aac9e4db8383f97c0f794106
Prince Avecillas <avecillasprince@yahoo.com>
2023-06-27 10:52:19 -0400

    Adding more lessons

---
+++ SwiftUILessons/BC63Toolbar.swift
@@ -0,0 +1,20 @@
+//
+//  BC63Toolbar.swift
+//  SwiftUILessons
+//
+//  Created by Prince Avecillas on 6/26/23.
+//
+
+import SwiftUI
+
+struct BC63Toolbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BC63Toolbar_Previews: PreviewProvider {
+    static var previews: some View {
+        BC63Toolbar()
+    }
+}
