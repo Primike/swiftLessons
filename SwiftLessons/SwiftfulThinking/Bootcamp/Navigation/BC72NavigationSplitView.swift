4310a70da526f5d9aa063101094a0f8bbaec4332
Prince Avecillas <avecillasprince@yahoo.com>
2024-11-01 23:23:32 -0400

    Swiftful

--- SwiftLessons/SwiftfulThinking/Bootcamp/Navigation/BC72NavigationSplitView.swift
+++ SwiftLessons/SwiftfulThinking/Bootcamp/Navigation/BC72NavigationSplitView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct BC72NavigationSplitView: View {
+    
+    @State private var visibility: NavigationSplitViewVisibility = .all
+    @State private var selectedCategory: FoodCategory? = nil
+    @State private var selectedFruit: Fruit? = nil
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView(columnVisibility: $visibility) {
+            List(FoodCategory.allCases, id: \.rawValue, selection: $selectedCategory) { category in 
+                NavigationLink(category.rawValue.capitalized, value: category)
+            }
+            .navigationTitle("Categories")
+        } content: {
+            if let selectedCategory {
+                Group {
+                    switch selectedCategory {
+                    case .fruits:
+                        List(Fruit.allCases, id: \.rawValue, selection: $selectedFruit) { fruit in
+                            NavigationLink(fruit.rawValue.capitalized, value: fruit)
+                        }
+                    case .vegtables:
+                        EmptyView()
+                    case .meats:
+                        EmptyView()
+                    }
+                }
+            } else {
+                Text("Select a category to begin!")
+            }
+        } detail: {
+            if let selectedFruit {
+                Text("You selected: \(selectedFruit.rawValue)")
+                    .font(.largeTitle)
+                    .navigationTitle(selectedFruit.rawValue.capitalized)
+            } else {
+                Text("Select something!")
+            }
+        }
+        .navigationSplitViewStyle(.balanced)
     }
+}
+
+enum FoodCategory: String, CaseIterable {
+    case fruits, vegtables, meats
+}
+
+enum Fruit: String, CaseIterable {
+    case apple, banana, orange
 }
 
 #Preview {
